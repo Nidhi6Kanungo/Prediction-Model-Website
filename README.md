51817b5f9b5f285349bb27cc289e3808ef3a342a
Nidhi6Kanungo <86800719+Nidhi6Kanungo@users.noreply.github.com>
2024-03-16 11:34:44 +0530

    uploading old project

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# Prediction-Model-Website
-Prediction model using machine learning
+# Prediction Models 💻
+
+### Software Required
+
+1. [Github Account](https://github.com)
+2. [VS Code IDE](https://code.visualstudio.com/download)
+3. [Heroku Account](https://heroku.com)
+4. [Git CLI](https://git-scm.com/downloads)
+5. [Anaconda Jupyter](https://anaconda.org/anaconda/jupyter)
+
+### Technolgy used:
+
+1. Flask (Python)
+2. Machine Learning (Supervised Learning)
+3. HTML,CSS,Js
+4. Git
+
+# Models:
+
+## 1. [Car Price Prediction Model 🚗](https://github.com/dipesh9765/Prediction_Models/tree/main/website/Car)
+
+## 2. [Diabetes Prediction Model](https://github.com/dipesh9765/Prediction_Models/tree/main/website/Diabetes)
+
+## 3. [Calories Burnt Prediction Model 🏋️‍♀️](https://github.com/dipesh9765/Prediction_Models/tree/main/website/Exercise)
+
+## 4. [House Price Prediction Model 🏠](https://github.com/dipesh9765/Prediction_Models/tree/main/website/House)
+
+## 5. [Medical Insurance Price Prediction Model ⚕](https://github.com/dipesh9765/Prediction_Models/tree/main/website/Medical_Insurance)
+
+#### References:
+
+##### [Flask Documentation](https://flask.palletsprojects.com/en/2.3.x/)
+
+##### [Python Documentation](https://docs.python.org/3/)
+
+##### [Pandas Documentation](https://pandas.pydata.org/docs/)
+
+##### [Matplotlib Documentation](https://matplotlib.org/stable/index.html)
+
+##### [Numpy Documentation](https://numpy.org/doc/)
+
+##### [Sklearn Documentation](https://scikit-learn.org/)
+
+##### [Seaborn Documentation](https://seaborn.pydata.org/)
+
+##### [XGBoost Documentation](https://xgboost.readthedocs.io/)
---
+++ website/views.py
@@ -0,0 +1,175 @@
+import json
+import pickle
+
+from flask import Flask,Blueprint, request, app, jsonify, url_for, render_template
+import numpy as np
+import pandas as pd
+
+views =Blueprint('views', __name__)
+
+
+@views.route('/')
+def index():
+    return render_template('base.html')
+
+
+
+house_regmodel = pickle.load(open('./website/House/regmodel.pkl', 'rb'))
+house_scalar = pickle.load(open('./website/House/scaling.pkl', 'rb'))
+
+diabetes_classifier = pickle.load(open('./website/Diabetes/d_regmodel.pkl','rb'))
+diabetes_scalar = pickle.load(open('./website/Diabetes/d_scaling.pkl', 'rb'))
+
+
+car_regmodel = pickle.load(open('./website/Car/c_regmodel.pkl','rb'))
+car_scalar = pickle.load(open('./website/Car/c_scaling.pkl', 'rb'))
+
+medical_regmodel = pickle.load(open('./website/Medical_Insurance/m_regmodel.pkl','rb'))
+medical_scalar = pickle.load(open('./website/Medical_Insurance/m_scaling.pkl', 'rb'))
+
+calorie_regmodel = pickle.load(open('./website/Exercise/e_regmodel.pkl','rb'))
+calorie_scalar = pickle.load(open('./website/Exercise/e_scaling.pkl', 'rb'))
+
+
+
+@views.route('/House') 
+def house():
+    return render_template('house.html')
+
+@views.route('/Car') 
+def car():
+    return render_template('car.html')
+
+@views.route('/Diabetes') 
+def diabetes():
+    return render_template('diabetes.html')
+
+@views.route('/Medical-Insurance')
+def medical_insurance():
+    return render_template('medical.html')
+
+@views.route('/Calorie-Predictor')
+def calorie_predictor():
+    return render_template('exercise.html')
+
+
+#HOUSE PREDICT API
+@views.route('/House/predict_api', methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = house_scalar.transform(np.array(list(data.values())).reshape(1, -1))
+    output = house_regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+# HOUSE PREDICT POST METHOD
+@views.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = house_scalar.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = house_regmodel.predict(final_input)[0]
+    output = format(output,".2f")
+    return render_template("house.html", prediction_text="The House price prediction is {} Million $".format(output))
+
+
+#DIABETES PREDICT API
+@views.route('/Diabetes/d_predict_api', methods=['POST'])
+def d_predict_api():
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = diabetes_scalar.transform(np.array(list(data.values())).reshape(1, -1))
+    output = diabetes_classifier.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+# DIABETED PREDICT POST METHOD
+@views.route('/d_predict', methods=['POST'])
+def d_predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = diabetes_scalar.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = diabetes_classifier.predict(final_input)[0] 
+    print(output)
+    if output == 1:
+        return render_template("diabetes.html",prediction_text=" High chances of Diabetes" )
+    else:
+        return render_template("diabetes.html", prediction_text=" Low chances of Diabetes" )
+
+
+
+
+#CAR PREDICT API
+@views.route('/Car/c_predict_api', methods=['POST'])
+def c_predict_api():
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = car_scalar.transform(np.array(list(data.values())).reshape(1, -1))
+    output = car_regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+
+#CAR PREDICT POST METHOD
+@views.route('/c_predict', methods=['POST'])
+def c_predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = car_scalar.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = car_regmodel.predict(final_input)[0] 
+    print(output)
+    output = format(output,".2f")
+    return render_template("car.html", prediction_text="The predicted car price is ₹ {} Lakhs".format(output))
+
+
+#MEDICAL PREDICT API
+@views.route('/Medical/m_predict_api', methods=['POST'])
+def m_predict_api():
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = medical_scalar.transform(np.array(list(data.values())).reshape(1, -1))
+    output = medical_regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+
+#MEDICAL PREDICT POST METHOD
+@views.route('/m_predict', methods=['POST'])
+def m_predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = medical_scalar.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = medical_regmodel.predict(final_input)[0] 
+    print(output) 
+    output = format(output,".2f")
+    return render_template("medical.html", prediction_text="The Medical Insurance predicted  price is ₹ {}".format(output))
+
+
+#CALORIE PREDICT API
+@views.route('/Calorie-Predictor/e_predict_api', methods=['POST'])
+def e_predict_api():
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = calorie_scalar.transform(np.array(list(data.values())).reshape(1, -1))
+    output = calorie_regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+
+#CALORIE PREDICT POST METHOD
+@views.route('/e_predict', methods=['POST'])
+def e_predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = calorie_scalar.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = calorie_regmodel.predict(final_input)[0] 
+    print(output) 
+    output = format(output,".2f")
+    return render_template("exercise.html", prediction_text="Calorie Burnt :  {} Kcal".format(output))
+
